f4bf5fb0d1d4e0cad2d6ddcc167d66435f3f9cc9
jahanabir8 <abir.opediatech@gmail.com>
2023-05-24 22:33:55 +0600

    newsletter

--- script.js
+++ script.js
@@ -59,3 +59,16 @@ $(".count__down__section")
   );
 });
 // ==========
+
+// testimonial swiper
+var swiper = new Swiper(".testi__swiper", {
+  zoom: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
